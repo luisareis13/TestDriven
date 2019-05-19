aa4ab1b57f3220c171ed92c3ce8601b3c2480334
unknown <luhr_eis@hotmail.com>
2019-05-19 18:40:41 -0300

    parte 5 - final

--- src/testDriven/Money.java
+++ src/testDriven/Money.java
@@ -1,30 +1,31 @@
 package testDriven;
 
-abstract class Money  {
-	   protected int amount; 
-	   protected String currency;
+class Money  {
+	   private int amount; 
+	   private String currency;
 	   
+	   static Money dollar(int amount)  {
+		      return new Money(amount, "USD");
+		   }
+			
+	   static Money franc(int amount) {
+		      return new Money(amount, "CHF");
+		   }
+		    
+	   Money(int amount, String currency) {
+		      this.amount = amount;
+		      this.currency = currency;
+		   }
+			
 	   public boolean equals(Object object) {
 		      Money money = (Money) object;
-		      return amount == money.amount && getClass().equals(money.getClass());
+		      return amount == money.amount && currency().equals(money.currency());
 		   }
-
-	   static Money dollar(int amount)  {
-	      return new Dollar(amount, "USD");
-	   }
-
-	   static Money franc(int amount) {
-	      return new Franc(amount, "CHF");
-	   }
-
-	   Money(int amount, String currency) {
-	      this.amount = amount;
-	      this.currency = currency;
-	   }
-	   
+			
+	   Money times(int multiplier) {
+		      return new Money(amount * multiplier, currency);
+		   }
 	   String currency() {
 		      return currency;
 		   }
-			
-	   abstract Money times(int multiplier); 
 	}
--- src/testDriven/MoneyTest.java
+++ src/testDriven/MoneyTest.java
@@ -8,10 +8,8 @@ class MoneyTest {
 
 	@Test
 	public void testEquality() {
-		   assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+		   assertTrue(Money.dollar(5).equals(Money.dollar(5))); 
 		   assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-		   assertTrue(Money.franc(5).equals(Money.franc(5)));
-		   assertFalse(Money.franc(5).equals(Money.franc(6)));
 		   assertFalse(Money.franc(5).equals(Money.dollar(5)));
 		}
 	@Test
@@ -19,12 +17,6 @@ class MoneyTest {
 		   Money five = Money.dollar(5);
 		   assertEquals(Money.dollar(10), five.times(2));
 		   assertEquals(Money.dollar(15), five.times(3));
-		}
-	@Test
-	public void testFrancMultiplication() {
-		   Money five = Money.franc(5);
-		   assertEquals(Money.franc(10), five.times(2));
-		   assertEquals(Money.franc(15), five.times(3));
 		}
 	@Test
 	public void testCurrency() {
